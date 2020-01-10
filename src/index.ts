dc485a331fbc78c91c63e887ab140c7210caf4a0
Dunice-0141 <dunice@Mac-mini-Dunice-0141.local>
2020-01-10 12:30:03 +0300

    Linter

--- src/index.ts
+++ src/index.ts
@@ -1,136 +1,144 @@
 import * as http from 'http';
+// TODO: determine if origin uses HTTPS
 // import * as https from 'https';
 
-import {
-    AllocationsResponse,
-    IRATypeResponce,
-    RiskValueResponce,
-    KeyPair,
-} from './types';
-import {createCipher} from "crypto";
+import { AllocationsResponse, IRATypeResponse, KeyPair, RiskValueResponse } from './types';
 
 export class BuildUp {
-    private readonly API_ORIGIN: string = 'localhost';
-    private readonly API_PORT: number = 6100;
+  private readonly API_ORIGIN: string = 'localhost';
+  private readonly API_PORT: number = 6100;
 
-    private readonly KEY: string;
-    private readonly SECRET: string;
+  private readonly KEY: string;
+  private readonly SECRET: string;
 
-    constructor(keyPair: KeyPair) {
-        this.KEY = keyPair.key;
-        this.SECRET = keyPair.secret;
+  constructor(keyPair: KeyPair) {
+    this.KEY = keyPair.key;
+    this.SECRET = keyPair.secret;
+  }
+
+  /**
+   *
+   * @param riskValue
+   */
+  public getAllocations(riskValue: number): Promise<AllocationsResponse> {
+    if (!riskValue) {
+      throw new Error('BuildUp: Risk Value is required!');
     }
+    return new Promise((resolve, reject) =>
+      http
+        .get(
+          {
+            hostname: this.API_ORIGIN,
+            method: 'GET',
+            path: `/api/v1/allocations?riskValue=${riskValue}&${this.getKeyAndSecret()}`,
+            port: this.API_PORT,
+          },
+          (response: http.IncomingMessage): void => {
+            let data: string = '';
+            response.setEncoding('utf8');
+            response.on('readable', (): void => {
+              const chunk: string = response.read();
+              if (chunk !== null) {
+                data += chunk;
+              }
+            });
+            response.on('end', () => {
+              return resolve(JSON.parse(data));
+            });
+          },
+        )
+        .on('error', (error: Error): void => reject(error)),
+    );
+  }
 
-    /**
-     * Get key and secret path string
-     * @returns {string}
-     */
-    private getKeyAndSecret() {
-        return `key=${this.KEY}&secret=${this.SECRET}`;
-    }
+  /**
+   *
+   * @param IRAType
+   */
+  public getIRATypes(IRAType: string): Promise<IRATypeResponse> {
+    return new Promise((resolve, reject) => {
+      const reqData = JSON.stringify({ IRAType: IRAType });
+      const options = {
+        headers: {
+          'Content-Length': reqData.length,
+          'Content-Type': 'application/json',
+        },
+        hostname: this.API_ORIGIN,
+        method: 'POST',
+        path: `/api/v1/ira-type?${this.getKeyAndSecret()}`,
+        port: this.API_PORT,
+      };
+      const request = this.createHttpRequest(options, resolve);
+      request.on('error', (error: Error): void => reject(error));
+      request.write(reqData);
+      request.end();
+    });
+  }
 
-    public getAllocations(riskValue: number): Promise<AllocationsResponse> {
-        if (!riskValue) {
-            throw new Error('BuildUp: Risk Value is required!');
+  /**
+   *
+   * @param riskGrowth
+   * @param riskLevel
+   * @param riskLosses
+   * @param riskVolatility
+   */
+  public async getRiskValues(
+    riskGrowth: number,
+    riskLevel: number,
+    riskLosses: number,
+    riskVolatility: number,
+  ): Promise<RiskValueResponse> {
+    return new Promise((resolve, reject) => {
+      const reqData = JSON.stringify({
+        riskGrowth,
+        riskLevel,
+        riskLosses,
+        riskVolatility,
+      });
+      const options = {
+        headers: {
+          'Content-Length': reqData.length,
+          'Content-Type': 'application/json',
+        },
+        hostname: this.API_ORIGIN,
+        method: 'POST',
+        path: `/api/v1/risk-value?${this.getKeyAndSecret()}`,
+        port: this.API_PORT,
+      };
+      const request = this.createHttpRequest(options, resolve);
+      request.on('error', (error: Error): void => reject(error));
+      request.write(reqData);
+      request.end();
+    });
+  }
+
+  /**
+   * Get key and secret path string
+   * @returns {string}
+   */
+  private getKeyAndSecret() {
+    return `key=${this.KEY}&secret=${this.SECRET}`;
+  }
+
+  /**
+   * Create HTTP request
+   * @param options {http.RequestOptions} - request options
+   * @param resolve {Function} - resolve the Promise
+   * @returns {*}
+   */
+  private createHttpRequest(options: http.RequestOptions, resolve: any) {
+    return http.request(options, (response: http.IncomingMessage): void => {
+      let data: string = '';
+      response.setEncoding('utf8');
+      response.on('readable', (): void => {
+        const chunk: string = response.read();
+        if (chunk !== null) {
+          data += chunk;
         }
-            return new Promise((resolve, reject) =>
-                http
-                    .get(
-                        {
-                            hostname: this.API_ORIGIN,
-                            method: 'GET',
-                            path: `/api/v1/allocations?riskValue=${riskValue}&${this.getKeyAndSecret()}`,
-                            port: this.API_PORT,
-                        },
-                        (response: http.IncomingMessage): void => {
-                            let data: string = '';
-                            response.setEncoding('utf8');
-                            response.on('readable', (): void => {
-                                const chunk: string = response.read();
-                                if (chunk !== null) {
-                                    data += chunk;
-                                }
-                            });
-                            response.on('end', () => {
-                                return resolve(JSON.parse(data));
-                            });
-                        },
-                    )
-                    .on('error', (error): void => reject(error)),
-                );
-    }
-    public getIRATypes(IRAType: string): Promise<IRATypeResponce> {
-        return new Promise((resolve, reject) => {
-            const reqData = JSON.stringify({ 'IRAType': IRAType });
-            const options = {
-                headers: {
-                    'Content-Length': Buffer.byteLength(reqData),
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                hostname: this.API_ORIGIN,
-                method: 'POST',
-                path: `/api/v1/ira-type?${this.getKeyAndSecret()}`,
-                port: this.API_PORT,
-            };
-
-            const request = http.request(options, (response: http.IncomingMessage): void => {
-                let data: string = '';
-                response.setEncoding('utf8');
-                response.on('readable', (): void => {
-                    const chunk: string = response.read();
-                    if (chunk !== null) {
-                        data += chunk;
-                    }
-                });
-                response.on('end', () => {
-                    return resolve(JSON.parse(data));
-                });
-            });
-
-            request.on('error', (error: Error): void => reject(error));
-
-            request.write(reqData);
-            request.end();
-        });
-    }
-    public getRiskValues(riskGrowth: number, riskLevel: number, riskLosses: number, riskVolatility: number, ): Promise<RiskValueResponce> {
-        return new Promise((resolve, reject) => {
-            const reqData = JSON.stringify({
-                riskGrowth,
-                riskLevel,
-                riskLosses,
-                riskVolatility,
-            });
-            console.log('reqData', reqData);
-            const options = {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Content-Length': Buffer.byteLength(reqData),
-                },
-                hostname: this.API_ORIGIN,
-                method: 'POST',
-                path: `/api/v1/risk-value?${this.getKeyAndSecret()}`,
-                port: this.API_PORT,
-            };
-            console.log('options', options);
-            const request = http.request(options, (response: http.IncomingMessage): void => {
-                let data: string = '';
-                response.setEncoding('utf8');
-                response.on('readable', (): void => {
-                    const chunk: string = response.read();
-                    if (chunk !== null) {
-                        data += chunk;
-                    }
-                });
-                response.on('end', () => {
-                    return resolve(JSON.parse(data));
-                });
-            });
-
-            request.on('error', (error: Error): void => reject(error));
-
-            request.write(reqData);
-            request.end();
-        });
-    }
+      });
+      response.on('end', () => {
+        return resolve(JSON.parse(data));
+      });
+    });
+  }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,44 +1,47 @@
 export interface AllocationsObject {
-    SPAB: number;
-    VEA: number;
-    VOO: number;
-    VTWO: number;
+  SPAB: number;
+  VEA: number;
+  VOO: number;
+  VTWO: number;
 }
 
 export interface AllocationsResponse {
-    data: AllocationsObject,
-    datetime: number;
-    info: string;
-    misc: string;
-    request: string;
-    status: number;
+  data: AllocationsObject;
+  datetime: number;
+  info: string;
+  misc: string;
+  request: string;
+  status: number;
 }
 
 export interface IRATypeObject {
-    IRAType: string;
-    maxContribution: number;
+  IRAType: string;
+  maxContribution: string | number;
 }
-export interface IRATypeResponce {
-    data: IRATypeObject,
-    datetime: number;
-    info: string;
-    misc: string;
-    request: string;
-    status: number;
-}
-export interface RiskValueObject {
-    riskValue: string;
-}
-export interface RiskValueResponce {
-    data: RiskValueObject,
-    datetime: number;
-    info: string;
-    misc: string;
-    request: string;
-    status: number;
+
+export interface IRATypeResponse {
+  data: IRATypeObject;
+  datetime: number;
+  info: string;
+  misc: string;
+  request: string;
+  status: number;
 }
 
 export interface KeyPair {
-    key: string;
-    secret: string;
+  key: string;
+  secret: string;
+}
+
+export interface RiskValueObject {
+  riskValue: string;
+}
+
+export interface RiskValueResponse {
+  data: RiskValueObject;
+  datetime: number;
+  info: string;
+  misc: string;
+  request: string;
+  status: number;
 }
